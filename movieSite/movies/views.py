afc91d38fdc135549116d999c74699aa86c1eeca
kznar <kznartee16reg@gmail.com>
2021-06-21 17:03:37 +0300

    add html files

--- movieSite/movies/views.py
+++ movieSite/movies/views.py
@@ -6,14 +6,13 @@ from .models import Movie
 
 class MoviesView(View):
     """Список фильмов"""
-
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, 'movies/movies.html', {"movie_list": movies})
 
 
-# class MovieDetailView(View):
-#     """Полное описание фильма"""
-#     def get(self, request, pk):
-#         movie = Movie.objects.get(id=pk)
-#         return render(request, "movies/movie_detail.html", {"movie": movie})
+class MovieDetailView(View):
+    """Полное описание фильма"""
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
